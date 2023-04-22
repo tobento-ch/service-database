322d570deca1aaf0f7858d10742f7aaa3befa2b8
TOBENTOch <info@tobento.ch>
2023-04-22 08:26:15 +0200

    missing method added

--- src/Schema/IndexFactoryInterface.php
+++ src/Schema/IndexFactoryInterface.php
@@ -18,5 +18,20 @@ namespace Tobento\Service\Database\Schema;
  */
 interface IndexFactoryInterface
 {
-    //
+    /**
+     * Create a new Index.
+     *
+     * @param string $name The index name.
+     * @return IndexInterface
+     */
+    public function createIndex(string $name): IndexInterface;
+    
+    /**
+     * Create a new Index from array.
+     *
+     * @param array $index
+     * @return IndexInterface
+     * @throws CreateIndexException
+     */
+    public function createIndexFromArray(array $index): IndexInterface;
 }
